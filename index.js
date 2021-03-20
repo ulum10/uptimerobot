9e0ba89a6d85978e00ac564ca3c4cbd93534ccfe
ulum10 <80685756+ulum10@users.noreply.github.com>
2021-03-19 22:11:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://painted-lightning-sand.glitch.me"]
+const urls = ["https://spiffy-versed-packet.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
